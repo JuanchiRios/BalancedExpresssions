a11414181a2624fac10bc8bf93b8475df7841193
JuanchiRios <juan.rios@despegar.com>
2016-02-19 12:13:05 -0300

    formating code

--- src/main/java/balancedExpressions/Manager.java
+++ src/main/java/balancedExpressions/Manager.java
@@ -4,44 +4,45 @@ import java.util.EmptyStackException;
 import java.util.Stack;
 
 public class Manager {
-	
+
 	public boolean isValidExpression(String exp) {
 		Stack<String> stack = new Stack<String>();
-		
+
 		for (int i = 0, n = exp.length(); i < n; i++) {
-		    String c = exp.substring(i, i+1);
+			String c = exp.substring(i, i + 1);
 			if (isOpeningDelimeter(c)) {
 				stack.push(c);
-			}
-			else if (isClosingDelimeter(c) ) {
+			} else if (isClosingDelimeter(c)) {
 				try {
-					if(!stack.pop().equals(toOpeningDelimeter(c))){
+					if (!stack.pop().equals(toOpeningDelimeter(c))) {
 						return false;
 					}
 				} catch (EmptyStackException e) {
 					return false;
 				}
-				
+
 			}
 		}
-	
+
 		return stack.isEmpty();
 	}
-	
-	public boolean isOpeningDelimeter(String c) {		
+
+	public boolean isOpeningDelimeter(String c) {
 		return "[{(".contains(c);
 	}
+
 	public boolean isClosingDelimeter(String c) {
 		return "]})".contains(c);
 	}
-	
+
 	public String toOpeningDelimeter(String c) {
-		if(c.equals("]")) return "[";
-		else if(c.equals("}")) return "{";
-		else if(c.equals(")")) return "(";
+		if (c.equals("]"))
+			return "[";
+		else if (c.equals("}"))
+			return "{";
+		else if (c.equals(")"))
+			return "(";
 		return "Not a delimeter";
 	}
-	
-
 
 }
--- src/test/java/balancedExpressionsTest/BalancedExpressionsTest.java
+++ src/test/java/balancedExpressionsTest/BalancedExpressionsTest.java
@@ -5,65 +5,65 @@ import balancedExpressions.Manager;
 import org.junit.*;
 
 public class BalancedExpressionsTest {
-	
+
 	private Manager expressionsManager = new Manager();
-	
+
 	@Test
 	public void isClosingDelimeter() {
 		assertTrue(expressionsManager.isClosingDelimeter("}"));
 		assertTrue(expressionsManager.isClosingDelimeter("]"));
 		assertTrue(expressionsManager.isClosingDelimeter(")"));
-	
+
 	}
-	
+
 	@Test
 	public void isOpeningDelimeterTest() {
 		assertTrue(expressionsManager.isOpeningDelimeter("("));
 		assertTrue(expressionsManager.isOpeningDelimeter("["));
 		assertTrue(expressionsManager.isOpeningDelimeter("{"));
-	
+
 	}
-	
+
 	@Test
 	public void toOpeningDelimeter() {
-		assertEquals(expressionsManager.toOpeningDelimeter("}"),"{");
-		assertEquals(expressionsManager.toOpeningDelimeter("Sarlompa"),"Not a delimeter");
-		assertEquals(expressionsManager.toOpeningDelimeter("]"),"[");
-		assertEquals(expressionsManager.toOpeningDelimeter(")"),"(");
-	
+		assertEquals(expressionsManager.toOpeningDelimeter("}"), "{");
+		assertEquals(expressionsManager.toOpeningDelimeter("Sarlompa"), "Not a delimeter");
+		assertEquals(expressionsManager.toOpeningDelimeter("]"), "[");
+		assertEquals(expressionsManager.toOpeningDelimeter(")"), "(");
+
 	}
-	
+
 	@Test
 	public void goodExamplesShouldBeTrueTest() {
 		assertTrue(expressionsManager.isValidExpression("{aa(bb[]cc)(dd)}"));
 		assertTrue(expressionsManager.isValidExpression("([xxx]{zzz})"));
 		assertTrue(expressionsManager.isValidExpression("(()[]())"));
 		assertTrue(expressionsManager.isValidExpression("{x[y(zz)y[]]}"));
-	
+
 	}
-	
+
 	@Test
 	public void badExamplesShouldBeFalseTest() {
-		
+
 		assertFalse(expressionsManager.isValidExpression("abc(d[)]"));
 		assertFalse(expressionsManager.isValidExpression("(({]))"));
 		assertFalse(expressionsManager.isValidExpression("a(b+c{)]"));
-	
+
 	}
-	
+
 	@Test
-	public void nullExpressionsShouldBeTrueTest() {	
-		assertTrue(expressionsManager.isValidExpression(""));	
+	public void nullExpressionsShouldBeTrueTest() {
+		assertTrue(expressionsManager.isValidExpression(""));
 	}
-	
+
 	@Test
-	public void onlyClosingDelimeterShouldBeFalse() {	
-		assertFalse(expressionsManager.isValidExpression("}])"));	
+	public void onlyClosingDelimeterShouldBeFalse() {
+		assertFalse(expressionsManager.isValidExpression("}])"));
 	}
-	
+
 	@Test
-	public void onlyOpeningDelimeterShouldBeFalse() {	
-		assertFalse(expressionsManager.isValidExpression("{[("));	
+	public void onlyOpeningDelimeterShouldBeFalse() {
+		assertFalse(expressionsManager.isValidExpression("{[("));
 	}
 
 }
